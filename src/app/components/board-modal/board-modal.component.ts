ecad658764455d813a05e6b4bfdb00e619e561e4
anda elaaser <andaelaaser@gmail.com>
2025-01-21 21:19:59 +0200

    subtask modal

--- src/app/components/board-modal/board-modal.component.ts
+++ src/app/components/board-modal/board-modal.component.ts
@@ -1,58 +1,90 @@
-import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, OnChanges, OnInit, Output, SimpleChanges, viewChild, ViewChild } from '@angular/core';
+import { ITask } from './../../interfaces/boardData';
+import { Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IBoardData } from '../../interfaces/boardData';
 import { ThemeService } from '../../services/theme.service';
-import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board-modal',
   standalone: true,
   imports: [
-    CommonModule,
     FormsModule,
-    ReactiveFormsModule],
+    ReactiveFormsModule,
+  CommonModule
+  ],
   templateUrl: './board-modal.component.html',
   styleUrl: './board-modal.component.scss'
 })
-export class BoardModalComponent implements OnChanges, OnInit {
+export class BoardModalComponent  {
   @Input() isBoardOpen: boolean = false;
   @Input() modalBoardTitle: string = '';
-  @Input() board: IBoardData = { name: '', columns: [] };
+  @Input() board: IBoardData | ITask | null = null;
   @Output() close = new EventEmitter<void>();
-  @Output() saveChanges =new EventEmitter<IBoardData>();
-@ViewChild('boardnameInput') boardnameInput!: ElementRef;
+  @Output() saveChanges = new EventEmitter<IBoardData | ITask>();
+  @ViewChild('boardnameInput') boardnameInput!: ElementRef;
   boardForm!: FormGroup;
-  isDarkMode: boolean =inject(ThemeService).isDarkMode
-  constructor(private fb: FormBuilder,
+  isDarkMode: boolean =inject(ThemeService).isDarkMode;
+  isBoard:boolean=false;
+  private themeSubscription!: Subscription;
+  constructor(
+    private fb: FormBuilder,
     private themeService: ThemeService
   ) { }
+  private isBoardData(board: IBoardData | ITask): board is IBoardData {
+    this.isBoard='name' in board;
+    return 'name' in board;
+  }
   ngOnInit(): void {
     this.boardForm = this.fb.group({
-      boardname: [this.board?.name || '', [Validators.required]],
+      boardname: [''],
       columns: this.fb.array([]),
+      title: [''],
+      subtasks: this.fb.array([])
     });
 
-    if (this.board?.columns?.length) {
-      this.board.columns.forEach(column => this.addColumn(column.name));
+    if (!this.board) return;
+
+    if (this.isBoardData(this.board)) {
+      this.boardForm = this.fb.group({
+        boardname: [this.board.name || '', Validators.required],
+        columns: this.fb.array(this.board.columns?.map(col => this.fb.control(col.name, Validators.required)) || [])
+      });
+    } else {
+      this.boardForm = this.fb.group({
+        title: [this.board.title || '', Validators.required],
+        subtasks: this.fb.array(this.board.subtasks?.map(sub => this.fb.control(sub.title, Validators.required)) || [])
+      });
     }
     this.themeService.isDarkMode$.subscribe((isDark) => {
       this.isDarkMode = isDark;
     });
   }
- ngOnChanges(changes: SimpleChanges): void {// focusing on input
-  if (changes['isBoardOpen'] && this.isBoardOpen) {
-    setTimeout(() => {
-      this.boardnameInput.nativeElement.focus();
-    }, 0); 
-  }
- }
+  // ngOnChanges(): void {
+  //   if (this.isBoardOpen) {
+  //     setTimeout(() => this.boardnameInput?.nativeElement.focus(), 0);
+  //   }
+  // }
+
+  // ngOnDestroy(): void {
+  //   if (this.themeSubscription) {
+  //     this.themeSubscription.unsubscribe();
+  //   }
+  // }
   get columns() {
-    return this.boardForm.get('columns') as FormArray;// to push in it
+    return (this.boardForm?.get('columns') as FormArray) || this.fb.array([]);
+  }
+
+  get subtasks() {
+    return (this.boardForm?.get('subtasks') as FormArray) || this.fb.array([]);
   }
 
   addColumn(columnName: string = '') {
     this.columns.push(this.fb.control(columnName, Validators.required));
+  }
+  addSubtask(subtaskTitle: string = '') {
+    this.subtasks.push(this.fb.control(subtaskTitle, Validators.required));
   }
 
   get formControl() {
@@ -64,7 +96,7 @@ export class BoardModalComponent implements OnChanges, OnInit {
       console.log('Form Data:', this.boardForm.value);
       const updatedBoard = this.boardForm.value;
       console.log('Saved Board:', updatedBoard);
-      this.saveChanges.emit(updatedBoard);
+      this.saveChanges.emit(this.boardForm.value);
       this.closeModal();
     } else {
       console.log('Form is invalid');
@@ -73,8 +105,15 @@ export class BoardModalComponent implements OnChanges, OnInit {
   addNewColumn() {
     this.columns.push(this.fb.control(''));
   }
+  addNewSubTask(){
+    this.subtasks.push(this.fb.control(''));
+  }
   closeColumn(index: number) {
     this.columns.removeAt(index);
+  }
+  closeSubTask(index:number){
+    this.subtasks.removeAt(index);
+
   }
   closeModal() {
     this.isBoardOpen = false;
--- src/app/components/project-board/Components/subtasks-modal/subtasks-modal.component.ts
+++ src/app/components/project-board/Components/subtasks-modal/subtasks-modal.component.ts
@@ -1,25 +1,71 @@
 import { Component, inject, Input } from '@angular/core';
-import { ISubTask } from '../../../../interfaces/boardData';
-import { CommonModule } from '@angular/common';
+import { ICloumn, ISubTask, ITask } from '../../../../interfaces/boardData';
 import { ThemeService } from '../../../../services/theme.service';
+import { BoardModalComponent } from "../../../board-modal/board-modal.component";
+import { DeleteBoardComponent } from "../../../delete-board/delete-board.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-subtasks-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BoardModalComponent, DeleteBoardComponent],
   templateUrl: './subtasks-modal.component.html',
   styleUrl: './subtasks-modal.component.scss'
 })
 export class SubtasksModalComponent {
-  @Input() subTasks!:ISubTask[];
-  constructor(
-    private themeService: ThemeService
-  ){}
-  _isDarkMode:boolean=inject(ThemeService).isDarkMode;
+  @Input() subTasks!: ISubTask[];
+  @Input() columns !:ICloumn[];
+    // logoSrc: string = 'assets/images/logo-dark.svg';
+    constructor(
+      private themeService: ThemeService
+    ){}
+    _isDarkMode:boolean=inject(ThemeService).isDarkMode
   ngOnInit(): void {
     this.themeService.isDarkMode$.subscribe((isDark) => {
       this._isDarkMode = isDark;
+      // if (this._isDarkMode) {
+      //   console.log(this._isDarkMode);
+      //   this.logoSrc = 'assets/images/logo-light.svg';
+      // } else {
+      //   console.log(this._isDarkMode);
+      //   this.logoSrc = 'assets/images/logo-dark.svg';
+      // }
     });
   }
 
+
+  sendTask():ITask|null {
+    if (this.subTasks && this.subTasks.length > 0) {
+      // Assuming you want to return a task with a list of subtasks
+      return {
+          title: 'Main Task Title',  // Example title, you can adjust as needed
+          description: 'Main task description',  // Example description
+          status: 'In Progress',  // Example status, you can adjust as needed
+          subtasks: this.subTasks,  // Assuming subTasks is an array of ISubTask objects
+      } as ITask;
+  }
+  return null;
+  }
+  isTaskModalOpen: boolean = false;
+  modalTaskTitle: string = '';
+
+  openTaskModal(title: string) {
+    this.modalTaskTitle = title;
+    this.isTaskModalOpen = true;
+  }
+
+  closeBoardModal() {
+    this.isTaskModalOpen = false;
+  }
+
+  isDeleteModalOpen: boolean = false;
+
+  deleteTaskModal(taskName: string, index: number = 0) {
+    this.modalTaskTitle = taskName;
+    this.isDeleteModalOpen = true;
+  }
+
+  closeDeleteTaskModal() {
+    this.isDeleteModalOpen = false;
+  }
 }
